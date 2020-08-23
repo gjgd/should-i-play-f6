6bd88a8fb7967d73fdd6c2467e05db9f16d3c10c
gjgd <gjgd@protonmail.com>
2020-08-23 22:18:35 +0200

    feat: first version that computes win rates

---
+++ main.py
@@ -0,0 +1,39 @@
+import re
+
+db_name = "lichess_db_standard_rated_2020-07.pgn"
+
+limit = 1e5
+i = 0
+games = 0
+games_where_white_played_f3 = {}
+games_where_black_played_f6 = {}
+with open(db_name) as f:
+    for line in f:
+        # Get the line in the PGN that lists the moves
+        if "1. " in line:
+            i += 1
+            if i > limit:
+                break
+            games += 1
+            line = line.rstrip()
+            # Stats about white playing f3
+            white_played_f3 = re.findall(r'\d\. f3', line)
+            score = line.split(' ')[-1]
+            if score not in ["1-0", "0-1", "1/2-1/2"]:
+                continue
+
+            if white_played_f3:
+                if score in games_where_white_played_f3:
+                    games_where_white_played_f3[score] += 1
+                else:
+                    games_where_white_played_f3[score] = 1
+            # Stats about black playing f6
+            black_played_f6 = re.findall(r'[^\.] f6', line)
+            if black_played_f6:
+                if score in games_where_black_played_f6:
+                    games_where_black_played_f6[score] += 1
+                else:
+                    games_where_black_played_f6[score] = 1
+
+
+print(games, games_where_white_played_f3, games_where_black_played_f6)
