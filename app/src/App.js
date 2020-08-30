a73085d5594b535dba0b6e28d0f3d782321ad340
gjgd <gjgd@protonmail.com>
2020-08-30 18:51:54 +0200

    chore: precompute white and black baseline

--- app/src/App.js
+++ app/src/App.js
@@ -17,6 +17,34 @@ const computeScore = (outcome) => {
   return score;
 };
 
+const blackAverage = Object.entries(data[BLACK]['*']).reduce((acc, [elo, outcome]) => {
+  return {
+    ...acc,
+    [elo]: computeScore(outcome),
+  };
+}, {});
+const whiteAverage = Object.entries(data[WHITE]['*']).reduce((acc, [elo, outcome]) => {
+  return {
+    ...acc,
+    [elo]: computeScore(outcome),
+  };
+}, {});
+const blackTotal = Object.entries(data[BLACK]['*']).reduce((acc, [elo, outcome]) => {
+  const wins = outcome['1'] || 0;
+  const losses = outcome['0'] || 0;
+  const draws = outcome['1/2'] || 0;
+  return acc + wins + losses + draws;
+}, 0);
+const whiteTotal = Object.entries(data[WHITE]['*']).reduce((acc, [elo, outcome]) => {
+  const wins = outcome['1'] || 0;
+  const losses = outcome['0'] || 0;
+  const draws = outcome['1/2'] || 0;
+  return acc + wins + losses + draws;
+}, 0);
+if (whiteTotal !== blackTotal) {
+  throw new Error('totals are not matching');
+}
+
 function App() {
   const [color, setColor] = React.useState(WHITE);
   const [graphData, setGraphData] = React.useState(null);
@@ -29,9 +57,13 @@ function App() {
   const computeGraph = () => {
     const moveData = data[color][move] || {};
     const graphData = Object.entries(moveData).map(([elo, outcome]) => {
-      const averageScore = computeScore(data[color]['*'][elo]);
       const score = computeScore(outcome);
-      return [elo, score, averageScore];
+      if (color === WHITE) {
+        return [elo, score, whiteAverage[elo]];
+      }
+      if (color === BLACK) {
+        return [elo, score, blackAverage[elo]];
+      }
     });
     const firstRow = ['x', `games where ${color} played ${move}`, 'all games'];
     setGraphData([firstRow, ...graphData]);
